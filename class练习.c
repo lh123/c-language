cd505a515ad7832574d7f82c32e3bdf4bcafe7e7
lh123 <1585086582@qq.com>
2015-01-30 00:09:16 +0800

    增加功能

--- class练习.c
+++ class练习.c
@@ -5,6 +5,28 @@ class cube
 	private:
 	int a,b;
 	public:
-	cube(int =1int =1)
-	void show()
-	
+	cube(int x=1,int y=1)
+	void input();
+	void show();
+	int mianji();
+};
+cube::cube(int x,int y)
+{
+	a=x;
+	b=y;
+}
+void cube::input()
+{
+	cin >> a >> b;
+}
+int cube::mianji()
+{
+	return a*b;
+}
+int main()
+{
+	cube cu;
+	cu.input();
+	cu.show();
+	cu.mainji();
+}
