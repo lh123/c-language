165637191f9de1fddaaf62b9eb33f698cf30fd76
lh123 <1585086582@qq.com>
2015-01-30 16:20:30 +0800

    增加链表

--- 学生管理系统/student.c
+++ 学生管理系统/student.c
@@ -2,9 +2,8 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class student//申明学生类
+struct student//学生结构体
 {
-	private:
 	int num;//序号
 	string name;//姓名
 	int xyr;//幸运日
@@ -12,11 +11,17 @@ class student//申明学生类
 	int xd;//线代
 	int ty;//体育
 	int wl;//物理
+};
+class listnode//申明链表生类
+{
+	private:
+	student date;
 	public:
 	//student(int,string,int,string,int,int,int);//构造函数
 	void show();//显示每个成员的信息
 	int sorce(); //及格返回true 否则返回false
 	void input();//输入函数
+	student *next;//链表指针
 };
 /*student::student(int a,string b,int c,string d,int e,int f,int g)//定义构造函数
 {
@@ -30,7 +35,7 @@ class student//申明学生类
 }*/
 int student::sorce()//定义判断是否及格的函数
 {
-	if(xd<60||ty<60||wl<60)
+	if(date.xd<60||date.ty<60||date.wl<60)
 	{
 		return false;//不及格返回
 	}
@@ -39,23 +44,23 @@ int student::sorce()//定义判断是否及格的函数
 }
 void student::show() //显示函数
 {
-	cout << num << "\t" << name << "\t" << xyr << "\t";
-	cout << sr << "\t" << xd << "\t" << ty << "\t" << wl << "\t" << endl;
+	cout << date.num << "\t" << date.name << "\t" << date.xyr << "\t";
+	cout << date.sr << "\t" << date.xd << "\t" << date.ty << "\t" << date.wl << "\t" << endl;
 }
 void student::input()//输入函数
 {
 	cout << "请输入序号:";
-	cin >> num;
+	cin >> date.num;
 	cout << "请输入姓名:";
-	cin >> name;
+	cin >> date.name;
 	cout << "请输入幸运日:";
-	cin >> xyr;
+	cin >> date.xyr;
 	cout << "请输入生日:";
-	cin >> sr;
+	cin >> date.sr;
 	cout << "请输入线代:";
-	cin >> xd;
+	cin >> date.xd;
 	cout << "请输入体育:";
-	cin >> ty;
+	cin >> date.ty;
 	cout << "请输入物理:";
-	cin >> wl;
+	cin >> date.wl;
 }
